574e407ea4679a2189a48007aba959f661c9a97e
Godugu Veena <goduguveena@gmail.com>
2024-12-28 14:23:01 +0000

    Binary Search Tree Problems Runtime - 264 ms (82.92%), Memory - 0.0B (100.00%)

--- 0620-not-boring-movies/solution.sql
+++ 0620-not-boring-movies/solution.sql
@@ -1,7 +1,2 @@
 # Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE id % 2 = 1
-  AND description <> 'boring'
-ORDER BY rating DESC;
-
+select id,movie,description,rating from Cinema where id%2!=0 and description!='boring' order by rating desc;
