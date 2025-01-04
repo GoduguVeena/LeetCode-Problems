737976aa54379f677e8123a2796f5e1809603f4d
Godugu Veena <goduguveena@gmail.com>
2025-01-04 04:42:37 +0000

    [LeetCode Sync] Runtime - 150 ms (82.33%), Memory - 39.2 MB (52.47%)

---
+++ 0204-count-primes/solution.c
@@ -0,0 +1,32 @@
+    /*1. trick1 is to use square root of n.
+     2.  trick2 is not to use non-prime numbers as the step
+     3. trick3 is to use i*i as the start. 
+     4. trick4 is to use count-- in every loop, avoiding another traversal. */  
+int countPrimes(int n) {
+	if(n <= 2) return 0;
+	if(n == 3) return 1;
+	bool *prime= (bool*)malloc(sizeof(bool)*n);
+	int i=0,j=0;
+	int count = n-2;
+	int rt = sqrt(n);//trick1
+	for(j = 0; j < n; j++)
+	{
+		prime[j] = 1;
+	}
+	for(i = 2; i <= rt; i++)
+	{
+	     if (prime[i])//trick2
+	    {
+    	    for(j=i*i ; j<n ; j+=i)//trick3
+    	    {
+    	        if (prime[j])
+    	                {
+    	                   prime[j]=0;
+    	                   count--;//trick4
+    	                }
+    	    }
+	    }
+	}
+	free(prime);
+	return count;
+}
