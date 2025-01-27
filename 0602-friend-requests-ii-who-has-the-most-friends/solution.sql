5e7cb80b6a5bc70bac755f59d3433eb1d1953dbd
Godugu Veena <goduguveena@gmail.com>
2025-01-27 15:13:03 +0000

    [LeetCode Sync] Runtime - 522 ms (6.24%), Memory - 0.0B (100.00%)

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+WITH CTE AS(
+    SELECT requester_id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id FROM RequestAccepted
+)
+
+SELECT requester_id AS id,COUNT(*) AS num
+FROM CTE
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
