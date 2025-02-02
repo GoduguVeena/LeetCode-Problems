2beadbe04e79256350404580ad39394642a98caf
Godugu Veena <goduguveena@gmail.com>
2025-02-02 05:37:41 +0000

    Binary Search Tree Problems Runtime - 3 ms (8.69%), Memory - 17.8 MB (100.00%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.py
+++ 1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -1,4 +1,5 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        return sum(nums[i - 1] > v 
-        for i, v in enumerate(nums)) <= 1
+        return (cntD:=sum(y<x for x, y in pairwise(nums)))==0 or (cntD==1 and nums[-1]<=nums[0])
+
+        
