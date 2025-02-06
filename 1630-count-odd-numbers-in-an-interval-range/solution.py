ed7be1a0f131bcdc309dc81571c43a5f80fd708c
Godugu Veena <goduguveena@gmail.com>
2025-02-06 15:33:38 +0000

    [LeetCode Sync] Runtime - 40 ms (79.68%), Memory - 17.9 MB (99.98%)

--- 1630-count-odd-numbers-in-an-interval-range/solution.py
+++ 1630-count-odd-numbers-in-an-interval-range/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        return (high + 1) // 2 - (low // 2)
+        # Initialize odd with the number of even numbers between low and high.
+        odd = (high - low) // 2
+        
+        # If either low or high is odd, increment odd by 1.
+        if low % 2 == 1 or high % 2 == 1:
+            odd += 1
+        
+        # Return the number of odd numbers between low and high.
+        return odd
