c353f10cc25e1dcfd8e6b5ed4d65662494baa10e
Godugu Veena <goduguveena@gmail.com>
2025-03-05 14:06:04 +0000

    Binary Search Tree Problems Runtime - 0 ms (100.00%), Memory - 18.8 MB (100.00%)

--- 0206-reverse-linked-list/solution.py
+++ 0206-reverse-linked-list/solution.py
@@ -1,16 +1,11 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy=ListNode()
-        c=head
-        while c:
-            next=c.next
-            c.next=dummy.next
-            dummy.next=c
-            c=next
-        return dummy.next   
+        node = None
+
+        while head:
+            temp = head.next
+            head.next = node
+            node = head
+            head = temp
         
+        return node
