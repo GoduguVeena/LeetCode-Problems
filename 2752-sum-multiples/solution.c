5d352dfe31c466af5de692e23ec7a0a050677855
Godugu Veena <goduguveena@gmail.com>
2025-08-02 08:59:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (100.00%)

---
+++ 2752-sum-multiples/solution.c
@@ -0,0 +1,9 @@
+int sumOfMultiples(int n) {
+    int result = 0;
+    for(int i = 0 ; i <= n ; i ++){
+        if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+            result += i;
+        }
+    }
+    return result;
+}
