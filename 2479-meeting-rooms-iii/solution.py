a3a6acf82ab5d057f084e47b1ff9ca397053b850
Godugu Veena <goduguveena@gmail.com>
2025-12-27 11:53:21 +0000

    [LeetCode Sync] Runtime - 817 ms (5.13%), Memory - 51.1 MB (98.90%)

--- 2479-meeting-rooms-iii/solution.py
+++ 2479-meeting-rooms-iii/solution.py
@@ -1,26 +1,42 @@
-import heapq
-
 class Solution:
     def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
-        meetings.sort(key=lambda x: x[0])
-        free = list(range(n))
-        heapq.heapify(free)
-        busy = []
-        cnt = [0]*n
-        for start, e in meetings:
-            while busy and busy[0][0] <= start:
-                _, r = heapq.heappop(busy)
-                heapq.heappush(free, r)
-            dur = e - start
-            if free:
-                r = heapq.heappop(free)
-                end = e
+        meetings.sort()
+
+        count = [0] * n
+        timer = [0] * n
+
+        itr = 0
+
+        while itr < len(meetings):
+            start, end = meetings[itr]
+            dur = end - start
+
+            room = -1
+            earliest = 10**18
+            earliestRoom = -1
+
+            for i in range(n):
+                if timer[i] < earliest:
+                    earliest = timer[i]
+                    earliestRoom = i
+                if timer[i] <= start:
+                    room = i
+                    break
+
+            if room != -1:
+                timer[room] = end
+                count[room] += 1
             else:
-                t, r = heapq.heappop(busy)
-                end = t + dur
-            cnt[r] += 1
-            heapq.heappush(busy, (end, r))
-        m = max(cnt)
-        for i, val in enumerate(cnt):
-            if val == m:
-                return i
+                timer[earliestRoom] += dur
+                count[earliestRoom] += 1
+
+            itr += 1
+
+        maxv = 0
+        idx = 0
+        for i in range(n):
+            if count[i] > maxv:
+                maxv = count[i]
+                idx = i
+
+        return idx
