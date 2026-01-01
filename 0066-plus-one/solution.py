841f93d6923fe51134678c15e96ec325af005222
Godugu Veena <goduguveena@gmail.com>
2026-01-01 04:39:56 +0000

    Binary Search Tree Problems Runtime - 0 ms (100.00%), Memory - 17.1 MB (100.00%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-
+        carry = 1
         for i in range(len(digits) - 1, -1, -1):
-
-            if digits[i] + 1 != 10:
-                digits[i] += 1
-                return digits
-            
-            digits[i] = 0
-
-            if i == 0:
-                return [1] + digits
+            s = digits[i] + carry
+            digits[i] = s % 10
+            carry = s // 10
+        if carry:
+            digits = [1] + digits
+        return digits
