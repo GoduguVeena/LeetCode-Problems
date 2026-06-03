5d6e136a420744167057468a38a0d88c47c0f521
Godugu Veena <goduguveena@gmail.com>
2026-06-03 07:33:03 +0000

    [LeetCode Sync] Runtime - 264 ms (83.61%), Memory - 0.0B (100.00%)

--- 0620-not-boring-movies/solution.sql
+++ 0620-not-boring-movies/solution.sql
@@ -1,2 +1,7 @@
 # Write your MySQL query statement below
-select id,movie,description,rating from Cinema where id%2!=0 and description!='boring' order by rating desc;
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1
+  AND description <> 'boring'
+ORDER BY rating DESC;
+
