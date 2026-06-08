040293a81768e8576e333c240af064b0ad155336
Godugu Veena <goduguveena@gmail.com>
2026-06-08 06:51:13 +0000

    [LeetCode Sync] Runtime - 16 ms (94.54%), Memory - 33.8 MB (57.56%)

--- 2265-partition-array-according-to-given-pivot/solution.py
+++ 2265-partition-array-according-to-given-pivot/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        result = [0] * len(nums)
-        left = 0
-        right = len(nums) - 1
-        
-        for i, j in zip(range(len(nums)), range(len(nums) - 1, -1, -1)):
-            if nums[i] < pivot:
-                result[left] = nums[i]
-                left += 1
-            
-            if nums[j] > pivot:
-                result[right] = nums[j]
-                right -= 1
-        
-        while left <= right:
-            result[left] = pivot
-            left += 1
-            
-        return result
+        less = []
+        equal = []
+        greater = []
+
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num > pivot:
+                greater.append(num)
+            else:
+                equal.append(num)
+
+        less.extend(equal)
+        less.extend(greater)
+
+        return less
