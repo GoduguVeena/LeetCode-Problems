dfd9e960f0fa1b71d02053202841b105e2978801
Godugu Veena <goduguveena@gmail.com>
2026-08-10 10:48:51 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 48.2 MB (22.23%)

---
+++ 0260-single-number-iii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+     int xor=0;
+     for(int num:nums){
+        xor^=num;
+     }
+    xor &= -xor;
+    int[] ans={0,0};
+    for(int num:nums){
+        if((num&xor)==0){
+            ans[0]^=num;
+        }
+        else ans[1]^=num;
+    }
+    return ans;
+}
+}
